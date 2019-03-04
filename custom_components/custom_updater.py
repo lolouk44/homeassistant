bf58f47ee8218dff677deacf2fcfda2fe66bd2dc
lolouk44 <laurent.mahe@gmail.com>
2019-03-04 17:47:33 +0000

    0.88.2 Changes

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -10,15 +10,16 @@ from datetime import timedelta
 
 import voluptuous as vol
 from aiohttp import web
+from homeassistant.const import EVENT_HOMEASSISTANT_START
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.2.11'
+VERSION = '4.2.16'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyupdate==1.3.1']
+REQUIREMENTS = ['pyupdate==1.3.5']
 
 CONF_TRACK = 'track'
 CONF_HIDE_SENSOR = 'hide_sensor'
@@ -78,7 +79,7 @@ async def async_setup(hass, config):
         card_controller = CustomCards(
             hass, conf_hide_sensor, conf_card_urls, conf_mode)
 
-        await card_controller.extra_init()
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, card_controller.extra_init())
 
         async_track_time_interval(
             hass, card_controller.force_reload, INTERVAL)
@@ -87,7 +88,7 @@ async def async_setup(hass, config):
         components_controller = CustomComponents(
             hass, conf_hide_sensor, conf_component_urls)
 
-        await components_controller.extra_init()
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, components_controller.extra_init())
 
         async_track_time_interval(
             hass, components_controller.cache_versions, INTERVAL)
@@ -96,7 +97,7 @@ async def async_setup(hass, config):
         python_scripts_controller = CustomPythonScripts(
             hass, conf_hide_sensor, conf_py_script_urls)
 
-        await python_scripts_controller.extra_init()
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, python_scripts_controller.extra_init())
 
         async_track_time_interval(
             hass, python_scripts_controller.cache_versions, INTERVAL)
@@ -158,7 +159,7 @@ class CustomCards():
         await self.cache_versions()
         await self.serve_dynamic_files()
 
-    async def force_reload(self):
+    async def force_reload(self, now=None):
         """Force data refresh"""
         _LOGGER.debug('CustomCards - force_reload')
         await self.pyupdate.force_reload()
